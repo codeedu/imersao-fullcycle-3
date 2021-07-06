8dd6c734e68f0beea515e1733ca7b6f7d2926785
Wesley Willians <wesleywillians@gmail.com>
2021-07-06 09:45:12 -0400

    update transaction

--- codebank/dto/transaction.go
+++ codebank/dto/transaction.go
@@ -3,14 +3,14 @@ package dto
 import "time"
 
 type Transaction struct {
-	ID              string
-	Name            string
-	Number          string
-	ExpirationMonth int32
-	ExpirationYear  int32
-	CVV             int32
-	Amount          float64
-	Store           string
-	Description     string
-	CreatedAt       time.Time
+	ID              string `json:"transaction_id"`
+	Name            string `json:"-"`
+	Number          string `json:"credit_card_number"`
+	ExpirationMonth int32 `json:"-"`
+	ExpirationYear  int32 `json:"-"`
+	CVV             int32 `json:"-"`
+	Amount          float64 `json:"amount"`
+	Store           string `json:"store"`
+	Description     string `json:"description"`
+	CreatedAt       time.Time `json:"payment_date"`
 }
