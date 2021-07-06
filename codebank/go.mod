17d4c9a8467f961130ef3c37b0fc2acca9c97d85
Wesley Willians <wesleywillians@gmail.com>
2021-07-06 09:47:17 -0400

    merging aula2

--- codebank/go.mod
+++ codebank/go.mod
@@ -3,7 +3,12 @@ module github.com/codeedu/codebank
 go 1.15
 
 require (
+	github.com/confluentinc/confluent-kafka-go v1.7.0
+	github.com/golang/protobuf v1.5.2
+	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.10.2
 	github.com/satori/go.uuid v1.2.0
+	google.golang.org/grpc v1.39.0
+	google.golang.org/protobuf v1.27.1
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- codebank/main.go
+++ codebank/main.go
@@ -3,46 +3,51 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"github.com/codeedu/codebank/domain"
+	"github.com/codeedu/codebank/infrastructure/grpc/server"
+	"github.com/codeedu/codebank/infrastructure/kafka"
 	"github.com/codeedu/codebank/infrastructure/repository"
 	"github.com/codeedu/codebank/usecase"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
+	"os"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
 
 func main() {
 	db := setupDb()
 	defer db.Close()
-
-	cc := domain.NewCreditCard()
-	cc.Number = "1234"
-	cc.Name = "Wesley"
-	cc.ExpirationYear = 2021
-	cc.ExpirationMonth = 7
-	cc.CVV = 123
-	cc.Limit = 1000
-	cc.Balance = 0
-
-	repo := repository.NewTransactionRepositoryDb(db)
-	err := repo.CreateCreditCard(*cc)
-	if err != nil {
-		fmt.Println(err)
-	}
+	producer := setupKafkaProducer()
+	processTransactionUseCase := setupTransactionUseCase(db, producer)
+	serveGrpc(processTransactionUseCase)
 }
 
-func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
+func setupTransactionUseCase(db *sql.DB, producer kafka.KafkaProducer) usecase.UseCaseTransaction {
 	transactionRepository := repository.NewTransactionRepositoryDb(db)
 	useCase := usecase.NewUseCaseTransaction(transactionRepository)
+	useCase.KafkaProducer = producer
 	return useCase
+}
+
+func setupKafkaProducer() kafka.KafkaProducer {
+	producer := kafka.NewKafkaProducer()
+	producer.SetupProducer(os.Getenv("KafkaBootstrapServers"))
+	return producer
 }
 
 func setupDb() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		"db",
-		"5432",
-		"postgres",
-		"root",
-		"codebank",
+		os.Getenv("host"),
+		os.Getenv("port"),
+		os.Getenv("user"),
+		os.Getenv("password"),
+		os.Getenv("dbname"),
 	)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
@@ -50,3 +55,26 @@ func setupDb() *sql.DB {
 	}
 	return db
 }
+
+func serveGrpc(processTransactionUseCase usecase.UseCaseTransaction) {
+	grpcServer := server.NewGRPCServer()
+	grpcServer.ProcessTransactionUseCase = processTransactionUseCase
+	fmt.Println("Rodando gRPC Server")
+	grpcServer.Serve()
+}
+
+//
+//cc := domain.NewCreditCard()
+//cc.Number = "1234"
+//cc.Name = "Wesley"
+//cc.ExpirationYear = 2021
+//cc.ExpirationMonth = 7
+//cc.CVV = 123
+//cc.Limit = 1000
+//cc.Balance = 0
+//
+//repo := repository.NewTransactionRepositoryDb(db)
+//err := repo.CreateCreditCard(*cc)
+//if err != nil {
+//fmt.Println(err)
+//}
